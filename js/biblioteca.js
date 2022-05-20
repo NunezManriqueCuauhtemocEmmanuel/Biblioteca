cab5bfe0f8cb38b7773b64277ad3d1feae9c4217
Nuñez Manrique Cuauhtémoc Emmanuel <76032890+NunezManriqueCuauhtemocEmmanuel@users.noreply.github.com>
2022-05-20 04:31:55 -0500

    biblioteca

---
+++ js/biblioteca.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        shadow: true,
+        translate: [0, 0, -400],
+      },
+      next: {
+        translate: ["100%", 0, 0],
+      },
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+});
